60045cd5c72c576aba1ae8ea17ab1b6aa959046a
orzeelekk <orzellpawell@gmail.com>
2022-12-16 02:29:32 +0100

    korekta headera

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -44,7 +44,7 @@ const Header = () => {
                                        "normal","1")}
                                    onMouseLeave={() => changeAnimation( "shake","0.08s",
                                        "alternate-reverse","infinite")}
-                                >{menu.title1}</a>
+                                >{menu.title}</a>
                             </li>
                         )
                     })}
